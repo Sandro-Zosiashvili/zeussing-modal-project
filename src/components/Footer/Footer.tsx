718bea699af0a3d9c7c4a517caea8f28e1229051
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2026-03-23 19:38:56 +0400

    Feat: button column

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,8 +12,8 @@ const Footer = () => {
             animate={{ y: 0, opacity: 1 }}
             transition={{duration: 0.5, ease: [0.22, 1, 0.36, 1], delay: 0.5}}
         >
-            <Button text={"Button"} isActive={true} />
-            <Button text={"Button"} isActive={false} />
+            <Button className={styles.Button} text={"Button"} isActive={true} />
+            <Button className={styles.Button} text={"Button"} isActive={false} />
         </motion.div>
     )
 }
